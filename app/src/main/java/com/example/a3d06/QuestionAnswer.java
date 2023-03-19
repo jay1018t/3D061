d6820b8de5648f2f4f2f475c1d61cb72d87dfa42
jay1018t <kktsangle350@gmail.com>
2023-03-19 05:09:44 +0000

    add quiz page

---
+++ app/src/main/java/com/example/a3d06/QuestionAnswer.java
@@ -0,0 +1,15 @@
+package com.example.a3d06;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+
+    };
+    public static String choices[][] ={
+
+    };
+    public static String correctAnswers[] ={
+
+    };
+
+}
